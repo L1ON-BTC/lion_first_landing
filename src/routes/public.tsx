6c8af87bbfad07308c3d822d84b47fcf0f083fdd
Kenneth Ashley <ken.u.diggitt@gmail.com>
2023-06-20 23:11:48 -0400

    add some routes

--- src/routes/public.tsx
+++ src/routes/public.tsx
@@ -2,8 +2,8 @@
 //const { AuthRoutes } = lazyImport(() => import('@/features/auth'), 'AuthRoutes');
 
 export const publicRoutes = [
-  {
-    path: '/about',
-    element: <div>about</div>,
-  },
+  { path: '/about', element: <div>about</div> },
+  { path: '/faq', element: <div>faq</div> },
+  { path: '/project/*', element: <div>project</div> },
+  { path: '/profile', element: <div>profiles</div> },
 ];
